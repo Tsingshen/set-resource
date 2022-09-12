edf9a1ea8eb5d02c17c982c78f69bf9086b06732
沈长青 <shenqinpeng@126.com>
2022-09-12 20:04:29 +0800

    Add auto reload

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"context"
 	"log"
-	"os"
 	"reflect"
-	"regexp"
 	"time"
 
 	"github.com/Tsingshen/k8scrd/client"
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
@@ -22,141 +22,175 @@ var (
 	CONTAINER_APP_NAME = "app"
 )
 
-type Resource struct {
-	CpuRequest string
-	MemRequest string
-	CpuLimit   string
-	MemLimit   string
+type LocalConfig struct {
+	Resource         configResource `yaml:"resource"`
+	IncludeNamespace []string       `yaml:"includeNamespace"`
+}
+
+type configResource struct {
+	Requests CpuAndMem `yaml:"requests"`
+	Limits   CpuAndMem `yaml:"limits"`
+}
+
+type CpuAndMem struct {
+	Cpu    string `yaml:"cpu"`
+	Memory string `yaml:"memory"`
 }
 
 func main() {
+	var lc = &LocalConfig{}
 	cs := client.GetClient()
+	ch := make(chan struct{})
+	// set viper
 
-	r := &Resource{
-		CpuRequest: "10m",
-		MemRequest: "56Mi",
-		CpuLimit:   "2000m",
-		MemLimit:   "2048Mi",
-	}
-	log.Printf("Set-resource Request: cpu=%s,mem=%s, Limit: cpu=%s,mem=%s\n", r.CpuRequest, r.MemRequest, r.CpuLimit, r.MemLimit)
-
-	err := watchDeploymentResource(cs, r)
+	viper.SetConfigName("config.yaml")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
 	if err != nil {
-		panic(err)
+		log.Panicf("fatal error config file: %v\n", err)
 	}
+
+	if err := viper.Unmarshal(lc); err != nil {
+		log.Panicf("unmarshal config err: %v\n", err)
+	}
+
+	go func() {
+		log.Printf("Set-resource Request: cpu=%s,mem=%s, Limit: cpu=%s,mem=%s\n", lc.Resource.Requests.Cpu, lc.Resource.Requests.Memory, lc.Resource.Limits.Cpu, lc.Resource.Limits.Memory)
+		err = watchDeploymentResource(cs, lc, ch)
+		if err != nil {
+			panic(err)
+		}
+
+	}()
+
+	viper.WatchConfig()
+	viper.OnConfigChange(func(in fsnotify.Event) {
+		if in.Op == fsnotify.Write {
+			if err := viper.Unmarshal(&lc); err != nil {
+				panic(err)
+			}
+			ch <- struct{}{}
+			go func() {
+				log.Printf("Set-resource Request: cpu=%s,mem=%s, Limit: cpu=%s,mem=%s\n", lc.Resource.Requests.Cpu, lc.Resource.Requests.Memory, lc.Resource.Limits.Cpu, lc.Resource.Limits.Memory)
+				err = watchDeploymentResource(cs, lc, ch)
+				if err != nil {
+					panic(err)
+				}
+			}()
+		}
+	})
+
+	select {}
 
 }
 
-func watchDeploymentResource(cs *kubernetes.Clientset, r *Resource) error {
-	informersFatory := informers.NewSharedInformerFactory(cs, time.Minute*2)
+func watchDeploymentResource(cs *kubernetes.Clientset, lc *LocalConfig, ch chan struct{}) error {
+	informersFatory := informers.NewSharedInformerFactory(cs, time.Minute*1)
 
 	deployInformer := informersFatory.Apps().V1().Deployments()
-	watchNs := os.Getenv("WATCH_NS")
+	watchNs := lc.IncludeNamespace
 
-	nsReg := regexp.MustCompile(`shencq`)
-	if watchNs != "" {
-		nsReg = regexp.MustCompile(watchNs)
-	}
-
-	deployInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc: func(obj interface{}) {
-			deploy := obj.(*appsv1.Deployment)
-			if nsReg.MatchString(deploy.Namespace) {
-				deployAnno := deploy.Annotations
-				v, ok := deployAnno["not-reset-resources"]
-				if ok {
-					if v == "true" {
-						log.Printf("deployment %s/%s set anno not-reset-resource, skip addFunc\n", deploy.Namespace, deploy.Name)
-						return
+	if watchNs != nil {
+		deployInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+			AddFunc: func(obj interface{}) {
+				deploy := obj.(*appsv1.Deployment)
+				if checkSliceIncludeStr(watchNs, deploy.Namespace) {
+					deployAnno := deploy.Annotations
+					v, ok := deployAnno["not-reset-resources"]
+					if ok {
+						if v == "true" {
+							log.Printf("deployment %s/%s set anno not-reset-resource, skip addFunc\n", deploy.Namespace, deploy.Name)
+							return
+						}
 					}
-				}
-				deployResource := getDeployResource(cs, deploy)
-				if !reflect.DeepEqual(deployResource, corev1.ResourceRequirements{}) {
-					if !checkResourceEqual(r, &deployResource) {
-						err := r.updateDeployResource(cs, deploy)
+					deployResource := getDeployResource(cs, deploy)
+					if !reflect.DeepEqual(deployResource, corev1.ResourceRequirements{}) {
+						if !checkResourceEqual(lc, &deployResource) {
+							err := lc.updateDeployResource(cs, deploy)
+							if err != nil {
+								log.Printf("Error: update deployment %s/%s resource err: %v\n", deploy.Namespace, deploy.Name, err)
+								return
+							}
+						}
+					} else {
+						err := lc.updateDeployResource(cs, deploy)
 						if err != nil {
 							log.Printf("Error: update deployment %s/%s resource err: %v\n", deploy.Namespace, deploy.Name, err)
 							return
 						}
 					}
-				} else {
-					err := r.updateDeployResource(cs, deploy)
-					if err != nil {
-						log.Printf("Error: update deployment %s/%s resource err: %v\n", deploy.Namespace, deploy.Name, err)
+
+				}
+
+			},
+			UpdateFunc: func(oldObj, newObj interface{}) {
+				oldDeploy := oldObj.(*appsv1.Deployment)
+				newDeploy := newObj.(*appsv1.Deployment)
+
+				if checkSliceIncludeStr(watchNs, oldDeploy.Namespace) {
+					deployAnno := newDeploy.Annotations
+					v, ok := deployAnno["not-reset-resources"]
+					if ok {
+						if v == "true" {
+							log.Printf("depoyment %s/%s set anno not-reset-resource, skip updateFunc\n", newDeploy.Namespace, newDeploy.Name)
+							return
+						}
+					}
+
+					newDeployResource := getDeployResource(cs, newDeploy)
+					oldDeployResource := getDeployResource(cs, oldDeploy)
+
+					if reflect.DeepEqual(newDeployResource, oldDeployResource) {
 						return
+					}
+
+					if reflect.DeepEqual(newDeployResource, corev1.ResourceRequirements{}) {
+						err := lc.updateDeployResource(cs, newDeploy)
+						if err != nil {
+							log.Printf("Error: UpdateFunc update deployemnt %s/%s err: %v\n", newDeploy.Namespace, newDeploy.Name, err)
+							return
+						}
+						return
+					}
+
+					if !checkResourceEqual(lc, &newDeployResource) {
+						err := lc.updateDeployResource(cs, newDeploy)
+						if err != nil {
+							log.Printf("Error: UpdateFunc update deployemnt %s/%s err: %v\n", newDeploy.Namespace, newDeploy.Name, err)
+							return
+						}
 					}
 				}
 
-			}
+			},
+		})
+		stopCh := make(chan struct{})
+		defer close(stopCh)
 
-		},
-		UpdateFunc: func(oldObj, newObj interface{}) {
-			oldDeploy := oldObj.(*appsv1.Deployment)
-			newDeploy := newObj.(*appsv1.Deployment)
+		// start informer
+		informersFatory.Start(stopCh)
+		informersFatory.WaitForCacheSync(stopCh)
 
-			if nsReg.MatchString(oldDeploy.Namespace) {
-				deployAnno := newDeploy.Annotations
-				v, ok := deployAnno["not-reset-resources"]
-				if ok {
-					if v == "true" {
-						log.Printf("depoyment %s/%s set anno not-reset-resource, skip updateFunc\n", newDeploy.Namespace, newDeploy.Name)
-						return
-					}
-				}
+		stopCh <- <-ch
 
-				newDeployResource := getDeployResource(cs, newDeploy)
-				oldDeployResource := getDeployResource(cs, oldDeploy)
-
-				if reflect.DeepEqual(newDeployResource, oldDeployResource) {
-					return
-				}
-
-				if reflect.DeepEqual(newDeployResource, corev1.ResourceRequirements{}) {
-					err := r.updateDeployResource(cs, newDeploy)
-					if err != nil {
-						log.Printf("Error: UpdateFunc update deployemnt %s/%s err: %v\n", newDeploy.Namespace, newDeploy.Name, err)
-						return
-					}
-					return
-				}
-
-				if !checkResourceEqual(r, &newDeployResource) {
-					err := r.updateDeployResource(cs, newDeploy)
-					if err != nil {
-						log.Printf("Error: UpdateFunc update deployemnt %s/%s err: %v\n", newDeploy.Namespace, newDeploy.Name, err)
-						return
-					}
-				}
-			}
-
-		},
-	})
-	stopCh := make(chan struct{})
-	defer close(stopCh)
-
-	// start informer
-	informersFatory.Start(stopCh)
-	informersFatory.WaitForCacheSync(stopCh)
-
-	<-stopCh
+	}
 
 	return nil
 
 }
 
-func checkResourceEqual(resource1 *Resource, resource2 *corev1.ResourceRequirements) bool {
+func checkResourceEqual(lc *LocalConfig, r2 *corev1.ResourceRequirements) bool {
 	r1 := &corev1.ResourceRequirements{
 		Limits: corev1.ResourceList{
-			corev1.ResourceCPU:    resource.MustParse(resource1.CpuLimit),
-			corev1.ResourceMemory: resource.MustParse(resource1.MemLimit),
+			corev1.ResourceCPU:    resource.MustParse(lc.Resource.Limits.Cpu),
+			corev1.ResourceMemory: resource.MustParse(lc.Resource.Limits.Memory),
 		},
 		Requests: corev1.ResourceList{
-			corev1.ResourceCPU:    resource.MustParse(resource1.CpuRequest),
-			corev1.ResourceMemory: resource.MustParse(resource1.MemRequest),
+			corev1.ResourceCPU:    resource.MustParse(lc.Resource.Requests.Cpu),
+			corev1.ResourceMemory: resource.MustParse(lc.Resource.Requests.Memory),
 		},
 	}
-
-	r2 := resource2
 
 	if r1 == nil && r2 != nil || r1 != nil && r2 == nil {
 		return false
@@ -187,7 +221,7 @@ func getDeployResource(cs *kubernetes.Clientset, d *appsv1.Deployment) corev1.Re
 	return corev1.ResourceRequirements{}
 }
 
-func (r *Resource) updateDeployResource(cs *kubernetes.Clientset, d *appsv1.Deployment) error {
+func (lc *LocalConfig) updateDeployResource(cs *kubernetes.Clientset, d *appsv1.Deployment) error {
 
 	// for the sake of loop run nothing
 	time.Sleep(time.Millisecond * 500)
@@ -199,12 +233,12 @@ func (r *Resource) updateDeployResource(cs *kubernetes.Clientset, d *appsv1.Depl
 		if v.Name == CONTAINER_APP_NAME {
 			c[k].Resources = corev1.ResourceRequirements{
 				Limits: corev1.ResourceList{
-					corev1.ResourceCPU:    resource.MustParse(r.CpuLimit),
-					corev1.ResourceMemory: resource.MustParse(r.MemLimit),
+					corev1.ResourceCPU:    resource.MustParse(lc.Resource.Limits.Cpu),
+					corev1.ResourceMemory: resource.MustParse(lc.Resource.Limits.Memory),
 				},
 				Requests: corev1.ResourceList{
-					corev1.ResourceCPU:    resource.MustParse(r.CpuRequest),
-					corev1.ResourceMemory: resource.MustParse(r.MemRequest),
+					corev1.ResourceCPU:    resource.MustParse(lc.Resource.Requests.Cpu),
+					corev1.ResourceMemory: resource.MustParse(lc.Resource.Requests.Memory),
 				},
 			}
 			_, err := cs.AppsV1().Deployments(dCopy.Namespace).Update(context.Background(), dCopy, metav1.UpdateOptions{
@@ -219,4 +253,18 @@ func (r *Resource) updateDeployResource(cs *kubernetes.Clientset, d *appsv1.Depl
 	}
 
 	return nil
+}
+
+func checkSliceIncludeStr(s []string, str string) bool {
+	if s == nil || str == "" {
+		return false
+	}
+
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+
+	return false
 }
